2628bb0938394cae8ae7e5fcefd20d3678733dd0
Goh See Ting <seeting@pitchspot.co>
2026-03-20 23:21:08 +0800

    fix: basePath for GitHub Pages

--- _next/static/aONAJP_1KkruQqs3-7JrD/_buildManifest.js
+++ _next/static/aONAJP_1KkruQqs3-7JrD/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/musterclub//_next/:path+",
+        "destination": "/musterclub/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
