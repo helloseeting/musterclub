38035496dec1b35446ac0580caced34983e4ecd6
Goh See Ting <seeting@pitchspot.co>
2026-03-21 00:01:57 +0800

    deploy: landing page

---
+++ _next/static/Mlua8ni8b6PLF6yZpTALh/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/musterclub//_next/:path+",
+        "destination": "/musterclub/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
